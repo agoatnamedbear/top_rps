42c7d127d0d5c74b8d89f473baa20be1d99fcc00
Sean Wildman <agoatnamedbear@gmail.com>
2024-02-12 13:18:34 -0500

    Write getComputerChoice function

---
+++ app.js
@@ -0,0 +1,12 @@
+const getComputerChoice = () => {
+    let randomNum = Math.floor(Math.random() * 3);
+    if (randomNum === 0) {
+        console.log('rock');
+    } else if (randomNum === 1) {
+        console.log('paper');
+    } else {
+        console.log('scissors');
+    }
+}
+
+getComputerChoice();
