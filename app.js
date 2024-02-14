040181aaa12cbb77572bed80a6bc58911f7736be
Sean Wildman <agoatnamedbear@gmail.com>
2024-02-14 12:47:55 -0500

    Format JS with prettier

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
 const getComputerChoice = () => {
-    let randomNum = Math.floor(Math.random() * 3);
-    if (randomNum === 0) {
-        return 'rock'
-    } else if (randomNum === 1) {
-        return 'paper';
-    } else {
-        return 'scissors';
-    }
-}
-
+  let randomNum = Math.floor(Math.random() * 3);
+  if (randomNum === 0) {
+    return "rock";
+  } else if (randomNum === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+};
 
 const playRound = (playerChoice, computerChoice) => {
-    computerChoice = getComputerChoice();
+  computerChoice = getComputerChoice();
 
-    playerChoice = prompt('Rock Paper or Scissors?').toLowerCase();
+  playerChoice = prompt("Rock Paper or Scissors?").toLowerCase();
 
-    if (playerChoice !== 'rock' && playerChoice !== 'paper' && playerChoice !== 'scissors') {
-    console.log('Invalid Choice! Please choose Rock, Paper or Scissors');
-    }
-
-    else if (playerChoice === computerChoice) {
-        console.log("It's a Tie!");
-    }
-
-    else if ((playerChoice === 'rock' && computerChoice === 'scissors') ||
-    (playerChoice === 'paper' && computerChoice === 'rock') ||
-    (playerChoice === 'scissors' && computerChoice === 'paper')) {
-        console.log(`Player Wins! ${playerChoice} beats ${computerChoice}`);
-    }
-
-    else {console.log(`Computer Wins! ${computerChoice} beats ${playerChoice}`);}
-}
+  if (
+    playerChoice !== "rock" &&
+    playerChoice !== "paper" &&
+    playerChoice !== "scissors"
+  ) {
+    console.log("Invalid Choice! Please choose Rock, Paper or Scissors");
+  } else if (playerChoice === computerChoice) {
+    console.log("It's a Tie!");
+  } else if (
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log(`Player Wins! ${playerChoice} beats ${computerChoice}`);
+  } else {
+    console.log(`Computer Wins! ${computerChoice} beats ${playerChoice}`);
+  }
+};
 
 const playGame = (playRound) => {
-    for (let i = 1; i <= 5 ; i++) {
-        playRound();
-    }
-}
+  for (let i = 1; i <= 5; i++) {
+    playRound();
+  }
+};
 
 playGame(playRound);
